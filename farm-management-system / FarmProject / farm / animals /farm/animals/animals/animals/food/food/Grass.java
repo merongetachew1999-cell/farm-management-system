9160d8f1a143a91f2618075b826972357a5163d1
Mary <merongetachew1999@gmail.com>
2026-05-18 15:49:25 +0300

    Create Grass.java

---
+++ farm-management-system / FarmProject / farm / animals /farm/animals/animals/animals/food/food/Grass.java
@@ -0,0 +1,8 @@
+package farm.food;
+
+public class Grass extends Food {
+    public Grass() { super("Grass"); }
+
+    @Override
+    public String getDescription() { return "Grass - good for cows."; }
+}
